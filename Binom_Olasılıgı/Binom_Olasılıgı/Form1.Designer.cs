a167eeb0cd52ade182a0f5b8e6a6d2fbd7a57ee7
umutramazanegi <78850577+umutramazanegi@users.noreply.github.com>
2021-12-27 21:41:23 +0300

    .

---
+++ Binom_Olasılıgı/Binom_Olasılıgı/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace Binom_Olasılıgı
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTrials = new System.Windows.Forms.TextBox();
+            this.txtSuccesses = new System.Windows.Forms.TextBox();
+            this.txtProbabilitySuccess = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBinomialProbability = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(1, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Deneme sayısı:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(1, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Başarı Sayısı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(1, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Başarı Olasılığı:";
+            // 
+            // txtTrials
+            // 
+            this.txtTrials.Location = new System.Drawing.Point(121, 27);
+            this.txtTrials.Name = "txtTrials";
+            this.txtTrials.Size = new System.Drawing.Size(100, 22);
+            this.txtTrials.TabIndex = 3;
+            // 
+            // txtSuccesses
+            // 
+            this.txtSuccesses.Location = new System.Drawing.Point(121, 68);
+            this.txtSuccesses.Name = "txtSuccesses";
+            this.txtSuccesses.Size = new System.Drawing.Size(100, 22);
+            this.txtSuccesses.TabIndex = 4;
+            // 
+            // txtProbabilitySuccess
+            // 
+            this.txtProbabilitySuccess.Location = new System.Drawing.Point(121, 105);
+            this.txtProbabilitySuccess.Name = "txtProbabilitySuccess";
+            this.txtProbabilitySuccess.Size = new System.Drawing.Size(100, 22);
+            this.txtProbabilitySuccess.TabIndex = 5;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCalculate.Location = new System.Drawing.Point(227, 71);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(108, 56);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Hesapla";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Binom_Olasılıgı.Properties.Resources.sd;
+            this.pictureBox1.Location = new System.Drawing.Point(-4, 143);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 20);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Binom_Olasılıgı.Properties.Resources.sd;
+            this.pictureBox2.Location = new System.Drawing.Point(181, 143);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(199, 20);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(1, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Binom Olasılığı:";
+            // 
+            // txtBinomialProbability
+            // 
+            this.txtBinomialProbability.Location = new System.Drawing.Point(121, 170);
+            this.txtBinomialProbability.Name = "txtBinomialProbability";
+            this.txtBinomialProbability.Size = new System.Drawing.Size(100, 22);
+            this.txtBinomialProbability.TabIndex = 10;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnClose.Location = new System.Drawing.Point(227, 168);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(108, 24);
+            this.btnClose.TabIndex = 11;
+            this.btnClose.Text = "Kapat";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Binom_Olasılıgı.Properties.Resources.sd;
+            this.pictureBox3.Location = new System.Drawing.Point(181, 198);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(199, 20);
+            this.pictureBox3.TabIndex = 13;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Binom_Olasılıgı.Properties.Resources.sd;
+            this.pictureBox4.Location = new System.Drawing.Point(-4, 198);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(197, 20);
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(201, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(179, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Ayşegül Eği 200117031";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(404, 244);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtBinomialProbability);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtProbabilitySuccess);
+            this.Controls.Add(this.txtSuccesses);
+            this.Controls.Add(this.txtTrials);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTrials;
+        private System.Windows.Forms.TextBox txtSuccesses;
+        private System.Windows.Forms.TextBox txtProbabilitySuccess;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBinomialProbability;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Binom_Olasılıgı/Binom_Olasılıgı/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Binom_Olasılıgı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private static long Factorial(long x)
+        {
+            if (x <= 1)
+                return 1;
+            else
+                return x * Factorial(x - 1);
+        }
+
+        private static long Combination(long a, long b)
+        {
+            if (a <= 1)
+                return 1;
+
+            return Factorial(a) / (Factorial(b) * Factorial(a - b));
+        }
+
+        private double BinomialProbability(int trials, int successes,
+                           double probabilityOfSuccess)
+        {
+            double probOfFailures = 1 - probabilityOfSuccess;
+
+            double c = Combination(trials, successes);
+            double px = Math.Pow(probabilityOfSuccess, successes);
+            double qnx = Math.Pow(probOfFailures, trials - successes);
+
+            return c * px * qnx;
+        }
+
+        
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int nbrOfTrials = 0, nbrOfSuccesses = 0;
+            double probOfSuccesses = 0.00;
+            double binomial = 0.00;
+
+            try
+            {
+                nbrOfTrials = int.Parse(txtTrials.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("The number of trials is not valid",
+                                "Binomial Probability");
+            }
+
+            try
+            {
+                nbrOfSuccesses = int.Parse(txtSuccesses.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("The number of successes is not valid",
+                                "Binomial Probability");
+            }
+
+            try
+            {
+                probOfSuccesses = double.Parse(txtProbabilitySuccess.Text);
+
+                binomial = BinomialProbability(nbrOfTrials,
+                                    nbrOfSuccesses,
+                                    probOfSuccesses);
+                txtBinomialProbability.Text = binomial.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("The number of trials is not valid",
+                                "Binomial Probability");
+            }
+        }
+    }
+}
+ 

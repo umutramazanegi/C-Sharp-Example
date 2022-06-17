e3a470caf5e496b1edbce9360ef0e72b4aa5bff7
Umut Ramazan Eği <78850577+umutramazanegi@users.noreply.github.com>
2022-06-18 00:41:28 +0300

    c#

---
+++ c#_gri_cevirme/Dijital_Goruntu_Cozumleme_Vize_Odevi/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace Dijital_Goruntu_Cozumleme_Vize_Odevi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.gri = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.kaydet = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(34, 89);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(327, 349);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // gri
+            // 
+            this.gri.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.gri.Location = new System.Drawing.Point(367, 206);
+            this.gri.Name = "gri";
+            this.gri.Size = new System.Drawing.Size(150, 45);
+            this.gri.TabIndex = 1;
+            this.gri.Text = "Gri Çevir";
+            this.gri.UseVisualStyleBackColor = true;
+            this.gri.Click += new System.EventHandler(this.gri_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(544, 89);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(307, 349);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Fotoğraf Eklemek İçin Tıkla";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(338, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(263, 51);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "    Dijital Görüntü Çözümleme Vize Ödevi\r\n         19701028 Umut Ramazan Eği\r\n\r\n";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(642, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "İşlenmiş Fotoğraf";
+            // 
+            // kaydet
+            // 
+            this.kaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.kaydet.Location = new System.Drawing.Point(368, 269);
+            this.kaydet.Name = "kaydet";
+            this.kaydet.Size = new System.Drawing.Size(149, 44);
+            this.kaydet.TabIndex = 6;
+            this.kaydet.Text = "Kaydet";
+            this.kaydet.UseVisualStyleBackColor = true;
+            this.kaydet.Click += new System.EventHandler(this.kaydet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 450);
+            this.Controls.Add(this.kaydet);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.gri);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button gri;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button kaydet;
+    }
+}
+
---
+++ c#_gri_cevirme/Dijital_Goruntu_Cozumleme_Vize_Odevi/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dijital_Goruntu_Cozumleme_Vize_Odevi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /*Fotoğrafı aç*/
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dosyaSecici = new OpenFileDialog();
+            dosyaSecici.Filter = "Jpeg Dosyası|*.jpeg;*.jpg|Bmp Dosyası|*.bmp";
+
+            if (dosyaSecici.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.ImageLocation = dosyaSecici.FileName;
+            }
+        }
+        /*Fotoğrafı gri yap*/
+
+        private Bitmap griyap(Bitmap cikisresim)
+        {
+            for (int i = 0; i < cikisresim.Height - 1; i++)
+            {
+                for (int j = 0; j < cikisresim.Width - 1; j++)
+                {
+                    int deger  =(cikisresim.GetPixel(j,i).R + cikisresim.GetPixel(j,i).G + cikisresim.GetPixel(j,i).B*0) /3;
+                    Color renk;
+                    renk = Color.FromArgb(deger, deger, deger);
+                    cikisresim.SetPixel(j, i, renk);
+                }
+            }
+            return cikisresim;
+        }
+
+        private void gri_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(pictureBox1.Image);
+            Bitmap gri = griyap(image);
+            pictureBox2.Image = gri;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        // Kaydet
+        private void kaydet_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog kaydet = new SaveFileDialog();
+            kaydet.Filter = "Jpeg Dosyası|*.jpeg;*.jpg|Bmp Dosyası|*.bmp";
+            if (DialogResult.OK == kaydet.ShowDialog())
+            {
+                this.pictureBox2.Image.Save(kaydet.FileName);
+            }
+
+        }
+    }
+}

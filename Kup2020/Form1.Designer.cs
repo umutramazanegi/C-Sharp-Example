276ded54158bd54e9519bbeb68595f93308c4505
Umut Ramazan Eği <78850577+umutramazanegi@users.noreply.github.com>
2022-06-25 17:25:53 +0300

    kup

---
+++ Kup2020/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Ornek2020
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.OpenGlControl = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.buttonAnimDurdur = new System.Windows.Forms.Button();
+            this.buttonAnimHizlandir = new System.Windows.Forms.Button();
+            this.buttonAnimBaslat = new System.Windows.Forms.Button();
+            this.timerDondur = new System.Windows.Forms.Timer(this.components);
+            this.labelHiz = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OpenGlControl
+            // 
+            this.OpenGlControl.AccumBits = ((byte)(0));
+            this.OpenGlControl.AutoCheckErrors = false;
+            this.OpenGlControl.AutoFinish = false;
+            this.OpenGlControl.AutoMakeCurrent = true;
+            this.OpenGlControl.AutoSwapBuffers = true;
+            this.OpenGlControl.BackColor = System.Drawing.Color.Black;
+            this.OpenGlControl.ColorBits = ((byte)(32));
+            this.OpenGlControl.DepthBits = ((byte)(16));
+            this.OpenGlControl.Dock = System.Windows.Forms.DockStyle.Left;
+            this.OpenGlControl.Location = new System.Drawing.Point(0, 0);
+            this.OpenGlControl.Name = "OpenGlControl";
+            this.OpenGlControl.Size = new System.Drawing.Size(400, 387);
+            this.OpenGlControl.StencilBits = ((byte)(0));
+            this.OpenGlControl.TabIndex = 0;
+            this.OpenGlControl.Load += new System.EventHandler(this.OpenGlControl_Load);
+            this.OpenGlControl.Paint += new System.Windows.Forms.PaintEventHandler(this.MyPaint);
+            this.OpenGlControl.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MyKeyDown);
+            // 
+            // buttonAnimDurdur
+            // 
+            this.buttonAnimDurdur.Location = new System.Drawing.Point(421, 85);
+            this.buttonAnimDurdur.Name = "buttonAnimDurdur";
+            this.buttonAnimDurdur.Size = new System.Drawing.Size(129, 54);
+            this.buttonAnimDurdur.TabIndex = 1;
+            this.buttonAnimDurdur.Text = "Anim Durdur";
+            this.buttonAnimDurdur.UseVisualStyleBackColor = true;
+            this.buttonAnimDurdur.Click += new System.EventHandler(this.buttonAnimDurdur_Click);
+            // 
+            // buttonAnimHizlandir
+            // 
+            this.buttonAnimHizlandir.Location = new System.Drawing.Point(421, 155);
+            this.buttonAnimHizlandir.Name = "buttonAnimHizlandir";
+            this.buttonAnimHizlandir.Size = new System.Drawing.Size(129, 54);
+            this.buttonAnimHizlandir.TabIndex = 2;
+            this.buttonAnimHizlandir.Text = "Anim Hızlandır";
+            this.buttonAnimHizlandir.UseVisualStyleBackColor = true;
+            this.buttonAnimHizlandir.Click += new System.EventHandler(this.buttonAnimHizlandir_Click);
+            // 
+            // buttonAnimBaslat
+            // 
+            this.buttonAnimBaslat.Location = new System.Drawing.Point(421, 12);
+            this.buttonAnimBaslat.Name = "buttonAnimBaslat";
+            this.buttonAnimBaslat.Size = new System.Drawing.Size(129, 54);
+            this.buttonAnimBaslat.TabIndex = 3;
+            this.buttonAnimBaslat.Text = "Anim Başlat";
+            this.buttonAnimBaslat.UseVisualStyleBackColor = true;
+            this.buttonAnimBaslat.Click += new System.EventHandler(this.buttonAnimBaslat_Click);
+            // 
+            // timerDondur
+            // 
+            this.timerDondur.Tick += new System.EventHandler(this.timerDondur_Tick);
+            // 
+            // labelHiz
+            // 
+            this.labelHiz.AutoSize = true;
+            this.labelHiz.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelHiz.Location = new System.Drawing.Point(438, 233);
+            this.labelHiz.Name = "labelHiz";
+            this.labelHiz.Size = new System.Drawing.Size(88, 26);
+            this.labelHiz.TabIndex = 4;
+            this.labelHiz.Text = "Hız = 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 387);
+            this.Controls.Add(this.labelHiz);
+            this.Controls.Add(this.buttonAnimBaslat);
+            this.Controls.Add(this.buttonAnimHizlandir);
+            this.Controls.Add(this.buttonAnimDurdur);
+            this.Controls.Add(this.OpenGlControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MyKeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl OpenGlControl;
+        private System.Windows.Forms.Button buttonAnimDurdur;
+        private System.Windows.Forms.Button buttonAnimHizlandir;
+        private System.Windows.Forms.Button buttonAnimBaslat;
+        private System.Windows.Forms.Timer timerDondur;
+        private System.Windows.Forms.Label labelHiz;
+    }
+}
+
---
+++ Kup2020/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tao.OpenGl;
+using Tao.Platform.Windows;
+
+namespace Ornek2020
+{
+    public partial class Form1 : Form
+    {
+        float rotX = 0;
+        float rotY = 0;
+        float rotZ = 0;
+        float hiz = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+            OpenGlControl.InitializeContexts();
+            // OpenGL ilk işlemler
+            Gl.glClearColor(0.0f, 0.0f, 0.5f, 0.0f);
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Gl.glOrtho(-2.0, 2.0, -2.0, 2.0, -2.0, 2.0);
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        float[][] pt = { 
+            new float[] { -1, -1, -1 }, new float[] { -1, 1, -1 },
+            new float[] {  1, -1, -1 }, new float[] {  1, 1, -1 },
+            new float[] { -1, -1,  1 }, new float[] { -1, 1,  1 },
+            new float[] {  1, -1,  1 }, new float[] {  1, 1,  1 }
+        };
+
+        public void quad(int n1, int n2, int n3, int n4)
+        {
+            Gl.glBegin(Gl.GL_QUADS);
+                Gl.glColor3fv(pt[n1]); Gl.glVertex3fv(pt[n1]);
+                Gl.glColor3fv(pt[n2]); Gl.glVertex3fv(pt[n2]);
+                Gl.glColor3fv(pt[n3]); Gl.glVertex3fv(pt[n3]);
+                Gl.glColor3fv(pt[n4]); Gl.glVertex3fv(pt[n4]);
+            Gl.glEnd();
+        }
+
+        public void cube()
+        {
+            quad(6, 2, 3, 7); quad(5, 1, 0, 4);
+            quad(7, 3, 1, 5); quad(4, 0, 2, 6);
+            quad(2, 0, 1, 3); quad(7, 5, 4, 6);
+        }
+
+        private void MyPaint(object sender, PaintEventArgs e)
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+            Gl.glShadeModel(Gl.GL_SMOOTH);
+
+            //Gl.glTranslatef(x,y,z);
+            Gl.glPushMatrix();
+                Gl.glRotatef(rotX, 1, 0, 0);
+                Gl.glRotatef(rotY, 0, 1, 0);
+                Gl.glRotatef(rotZ, 0, 0, 1);
+                cube();
+            Gl.glPopMatrix();
+        }
+
+        private void OpenGlControl_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MyKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+                rotZ = (rotZ + 5) % 360;
+            if (e.KeyCode == Keys.D)
+                rotZ = (rotZ - 5) % 360;
+            if (e.KeyCode == Keys.W)
+                rotX = (rotX + 5) % 360;
+            if (e.KeyCode == Keys.S)
+                rotX = (rotX - 5) % 360;
+            OpenGlControl.Refresh();
+        }
+
+        private void buttonZDondur_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAnimBaslat_Click(object sender, EventArgs e)
+        {
+            // timer Interval ile verilen msn hızla güncellenir
+            timerDondur.Interval = 50;
+            timerDondur.Start();
+            hiz = 1;
+            labelHiz.Text = "Hız = " + hiz;
+        }
+
+        private void buttonAnimHizlandir_Click(object sender, EventArgs e)
+        {
+            // Min 1, maks 9 olsun istedim.
+            hiz = (hiz + 1) % 10;
+            if (hiz == 0) hiz = 1;
+            labelHiz.Text = "Hız = " + hiz;
+        }
+
+        private void buttonAnimDurdur_Click(object sender, EventArgs e)
+        {
+            hiz = 0;
+            labelHiz.Text = "Hız = " + hiz;
+            timerDondur.Stop();
+        }
+
+        private void timerDondur_Tick(object sender, EventArgs e)
+        {
+            rotZ = (rotZ + hiz) % 360;
+            rotX = rotY = rotZ;
+            OpenGlControl.Refresh();
+        }
+    }
+}
